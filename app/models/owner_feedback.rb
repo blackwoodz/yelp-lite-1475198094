13c788b0e26f793aa3e6a0e06db1e80aae859a97
Raghu Betina <raghu@firstdraft.com>
2016-09-30 01:18:34 +0000

    Direct association between ownerships and owner_feedbacks

--- app/models/owner_feedback.rb
+++ app/models/owner_feedback.rb
@@ -1,6 +1,9 @@
 class OwnerFeedback < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "Ownership"
+
   belongs_to :review
 
   # Indirect associations
