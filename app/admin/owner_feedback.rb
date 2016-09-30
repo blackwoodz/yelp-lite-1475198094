426902c3ab0a1338ba6763a6bc6a9e44815d4d2b
Raghu Betina <raghu@firstdraft.com>
2016-09-30 01:19:22 +0000

    Whitelist columns in OwnerFeedback for ActiveAdmin

--- app/admin/owner_feedback.rb
+++ app/admin/owner_feedback.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register OwnerFeedback do
 
+ permit_params :review_id, :owner_id, :response
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
